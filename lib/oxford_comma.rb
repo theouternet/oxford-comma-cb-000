dd17caf16e1c8c08241921d0babaa017ccb1bd87
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-09-03 22:48:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
+if (array[0] != nil) && (array[1] == nil) 
+return array.join
+elsif (array[1] != nil) && (array[2] == nil) 
+array[0]="#{array[0]} and "
+return array.join
 
+elsif (array[2] != nil) && (array[3] == nil) 
+array[0]="#{array[0]}, "
+array[1]="#{array[1]}, and "
+return array.join
+
+elsif (array[3] != nil)
+
+array[-1]="and #{array[-1]}"
+return array.join(", ")
+
+end
 end
